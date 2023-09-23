dc52889441dd0cdae506c2b469de2e2e7910ba8e
maldavan5916 <grachovv12345@gmail.com>
2023-09-23 19:54:04 +0300

    Changes to be committed: 	modified:   main.py
     Changes not staged for commit:
    	deleted:    GitRep.txt
    	modified:   main.py
    	deleted:    students_bd.db
    
     Untracked files:
    	res/

--- main.py
+++ main.py
@@ -36,11 +36,17 @@ class MainApp(ctk.CTk):
         reports_menu = tk.Menu(self.menu_bar, tearoff=0)
         reports_menu.add_command(label="Создать Отчёт")
         self.menu_bar.add_cascade(label="Отчёты", menu=reports_menu)
+        
+        help_menu = tk.Menu(self.menu_bar, tearoff=0)
+        help_menu.add_command(label="Cправка")
+        help_menu.add_command(label="О программе")
+        self.menu_bar.add_cascade(label="Сервис", menu=help_menu)
 
         file_menu.configure(bg='#555', fg='white')
         references_menu.configure(bg='#555', fg='white')
         tables_menu.configure(bg='#555', fg='white')
         reports_menu.configure(bg='#555', fg='white')
+        help_menu.configure(bg='#555', fg='white')
 
         self.config(menu=self.menu_bar)
         
@@ -51,34 +57,5 @@ class MainApp(ctk.CTk):
         # Очищаем предыдущие виджеты (если есть)
         for widget in self.table_frame.winfo_children():
             widget.destroy()
-
-        # Создаем соединение с базой данных
-        conn = sqlite3.connect('students_bd.db')
-        cursor = conn.cursor()
-
-        # Выполняем SQL-запрос для выборки данных из таблицы students
-        cursor.execute("SELECT * FROM students")
-        data = cursor.fetchall()
-
-        # Закрываем соединение с базой данных
-        conn.close()
-
-         # Выводим заголовки таблицы
-        headers = ["ID", "ФИО", "Дата рождения", "Телефон", "Дата поступления", "Дата окончания", "Номер билета", "Курс", "Группа", "Отделение", "Пол", "Финансирование", "Специальность"]
-        for col, header in enumerate(headers):
-            label_frame = ctk.CTkFrame(self.table_frame)
-            label_frame.grid(row=0, column=col, padx=5, pady=5, sticky='nsew')
-            
-            label = ctk.CTkLabel(label_frame, text=header)
-            label.pack(padx=5, pady=5, fill='both', expand=True)
-
-        # Выводим данные
-        for row_num, row_data in enumerate(data, start=1):
-            for col, cell_data in enumerate(row_data):
-                label_frame = ctk.CTkFrame(self.table_frame)
-                label_frame.grid(row=row_num, column=col, padx=5, pady=5, sticky='nsew')
-                
-                label = ctk.CTkLabel(label_frame, text=str(cell_data))
-                label.pack(padx=5, pady=5, fill='both', expand=True)
 if __name__ == "__main__":
     MainApp().mainloop()
