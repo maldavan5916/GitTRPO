eb96969b056f1fb71371f68e7dd772215b43ccb6
maldavan5916 <grachovv12345@gmail.com>
2023-09-29 00:22:11 +0300

    Changes to be committed: 	modified:   main.py 	deleted:    res/bg.png 	new file:   res/images/bg0.png 	new file:   res/images/bg1.png 	new file:   res/images/bg2.png

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ class MainApp(ctk.CTk):
     def __init__(self):
         super().__init__()
 
-        # Создание основного окна приложения
         self.title('Студенческий отдел кадров')
-        self.geometry('1250x500')
+        self.geometry('865x450')
+        self.resizable(width=False, height=False)
+
+        # Создание фрейма для отображения таблицы
+        self.table_frame = ctk.CTkFrame(self, width=700, height=400)
+        self.table_frame.grid(row=0, column=0, padx=5, pady=5)
+
+        # Загрузка фона
+        bg = ctk.CTkImage(Image.open("res\\images\\bg2.png"), size=(700, 400))
+        lbl = ctk.CTkLabel(self.table_frame, image=bg, text='Таблица не открыта', font=("Calibri", 40))
+        lbl.place(relwidth=1, relheight=1)
 
         # Создание меню
         self.menu_bar = tk.Menu(self, background='#555', foreground='white')
@@ -18,29 +27,24 @@ class MainApp(ctk.CTk):
         # Меню "Файл"
         file_menu = tk.Menu(self.menu_bar, tearoff=0)
         file_menu.add_command(label="Сохранить")
-        file_menu.add_command(label="добавить запись")
-        file_menu.add_command(label="удалить запись")
-        file_menu.add_command(label="изменить запись")
-        file_menu.add_command(label="Поиск")
-        file_menu.add_command(label="Отмена")
         file_menu.add_separator()
         file_menu.add_command(label="Выход", command=self.quit)
         self.menu_bar.add_cascade(label="Файл", menu=file_menu)
 
         # Меню "Справочники"
         references_menu = tk.Menu(self.menu_bar, tearoff=0)
-        references_menu.add_command(label="Пол", command=lambda: self.show_tabl("SELECT * FROM pol"))
-        references_menu.add_command(label="Курс", command=lambda: self.show_tabl("SELECT * FROM kurs"))
-        references_menu.add_command(label="Группа", command=lambda: self.show_tabl("SELECT * FROM gruop"))
-        references_menu.add_command(label="Специальность", command=lambda: self.show_tabl("SELECT * FROM spec"))
-        references_menu.add_command(label="Отделение", command=lambda: self.show_tabl("SELECT * FROM otdelenie"))
-        references_menu.add_command(label="Вид финансирования", command=lambda: self.show_tabl("SELECT * FROM vid_finan"))
+        references_menu.add_command(label="Пол", command=lambda: self.show_table("SELECT * FROM pol"))
+        references_menu.add_command(label="Kypc", command=lambda: self.show_table("SELECT * FROM kurs"))
+        references_menu.add_command(label="Группа", command=lambda: self.show_table("SELECT * FROM gruop"))
+        references_menu.add_command(label="Специальность", command=lambda: self.show_table("SELECT * FROM spec"))
+        references_menu.add_command(label="Отделение", command=lambda: self.show_table("SELECT * FROM otdelenie"))
+        references_menu.add_command(label="Вид финансирования", command=lambda: self.show_table("SELECT * FROM vid_finan"))
         self.menu_bar.add_cascade(label="Справочники", menu=references_menu)
 
         # Меню "Таблицы"
         tables_menu = tk.Menu(self.menu_bar, tearoff=0)
-        tables_menu.add_command(label="Студенты", command=lambda: self.show_tabl("SELECT * FROM students"))
-        tables_menu.add_command(label="Родители", command=lambda: self.show_tabl("SELECT * FROM parents"))
+        tables_menu.add_command(label="Студенты", command=lambda: self.show_table("SELECT * FROM students"))
+        tables_menu.add_command(label="Родители", command=lambda: self.show_table("SELECT * FROM parents"))
         self.menu_bar.add_cascade(label="Таблицы", menu=tables_menu)
 
         # Меню "Отчёты"
@@ -51,7 +55,7 @@ class MainApp(ctk.CTk):
         # Меню "Сервис"
         help_menu = tk.Menu(self.menu_bar, tearoff=0)
         help_menu.add_command(label="Руководство пользователя")
-        help_menu.add_command(label="О программе")
+        help_menu.add_command(label="O программе")
         self.menu_bar.add_cascade(label="Сервис", menu=help_menu)
 
         # Настройка цветов меню
@@ -64,19 +68,28 @@ class MainApp(ctk.CTk):
         # Установка меню в главное окно
         self.config(menu=self.menu_bar)
 
-        # Создание фрейма для отображения таблицы и фона
-        self.table_frame = ctk.CTkFrame(self)
-        self.table_frame.pack(fill='both', expand=True)
+        btn_width = 150
+        pad = 5
 
-        # Загрузка фона
-        bg = ctk.CTkImage(Image.open("res\\bg.png"), size=(3840, 2400))
-        self.lbl = ctk.CTkLabel(self.table_frame, image=bg, text='Студенческий отдел кадров', font=("Calibri", 40))
-        self.lbl.pack(anchor='center', expand=1)
+        # Создание кнопок и виджетов для поиска и редактирования данных
+        btn_frame = ctk.CTkFrame(self)
+        btn_frame.grid(row=0, column=1)
+        ctk.CTkButton(btn_frame, text="добавить", width=btn_width).pack(pady=pad)
+        ctk.CTkButton(btn_frame, text="удалить", width=btn_width).pack(pady=pad)
+        ctk.CTkButton(btn_frame, text="изменить", width=btn_width).pack(pady=pad)
+        ctk.CTkButton(btn_frame, text="сохранить", width=btn_width).pack(pady=pad)
+        ctk.CTkButton(btn_frame, text="отмена", width=btn_width).pack(pady=pad)
 
-    def show_tabl(self, sql_query):
+        search_frame = ctk.CTkFrame(self)
+        search_frame.grid(row=1, column=0)
+        self.search_entry = ctk.CTkEntry(search_frame, width=300)
+        self.search_entry.grid(row=0, column=0, padx=pad)
+        ctk.CTkButton(search_frame, text="Поиск", width=20).grid(row=0, column=1, padx=pad)
+        ctk.CTkButton(search_frame, text="настроить поиск").grid(row=0, column=2, padx=pad)
+    
+    def show_table(self, sql_query):
         # Очистка фрейма перед отображением новых данных
-        for widget in self.table_frame.winfo_children():
-            widget.destroy()
+        for widget in self.table_frame.winfo_children(): widget.destroy()
 
         # Подключение к базе данных SQLite
         conn = sqlite3.connect("res\\students_bd.db")
@@ -91,35 +104,23 @@ class MainApp(ctk.CTk):
 
         # Закрытие соединения с базой данных
         conn.close()
+            
+        canvas = ctk.CTkCanvas(self.table_frame, width=865, height=480)
+        canvas.pack(fill="both", expand=True)
 
-        # Создание виджета Treeview для отображения таблицы
-        tree = ttk.Treeview(self.table_frame, columns=table_headers, show="headings")
-        for header in table_headers:
-            tree.heading(header, text=header)
-            tree.column(header, width=100)  # Установите ширину столбца по своему усмотрению
+        x_scrollbar = ttk.Scrollbar(self.table_frame, orient="horizontal", command=canvas.xview)
+        x_scrollbar.pack(side="bottom", fill="x")
 
-        for row in table_data:
-            tree.insert("", "end", values=row)
+        canvas.configure(xscrollcommand=x_scrollbar.set)
 
-        tree.grid(row=0, column=0)
+        table = ttk.Treeview(self.table_frame, columns=table_headers, show="headings")
+        for header in table_headers: table.heading(header, text=header)
+        for row in table_data: table.insert("", "end", values=row)
 
-        # Создание кнопок и виджетов для поиска и редактирования данных
-        btn_frame = ctk.CTkFrame(self.table_frame)
-        btn_frame.grid(row=0, column=1)
+        canvas.create_window((0, 0), window=table, anchor="nw")
 
-        btn_width = 150
-        pad = 5
-        ctk.CTkButton(btn_frame, text="добавить запись", width=btn_width).pack(pady=pad)
-        ctk.CTkButton(btn_frame, text="удалить запись", width=btn_width).pack(pady=pad)
-        ctk.CTkButton(btn_frame, text="изменить запись", width=btn_width).pack(pady=pad)
-        ctk.CTkButton(btn_frame, text="сохранить", width=btn_width).pack(pady=pad)
-        ctk.CTkButton(btn_frame, text="отмена", width=btn_width).pack(pady=pad)
-
-        search_frame = ctk.CTkFrame(self.table_frame)
-        search_frame.grid(row=1, column=0)
-        ctk.CTkEntry(search_frame, width=300).grid(row=0, column=0, padx=pad)
-        ctk.CTkButton(search_frame, text="Поиск", width=20).grid(row=0, column=1, padx=pad)
-        ctk.CTkButton(search_frame, text="настроить поиск").grid(row=0, column=2, padx=pad)
+        table.update_idletasks()
+        canvas.config(scrollregion=canvas.bbox("all"))
 
 if __name__ == "__main__":
     MainApp().mainloop()
